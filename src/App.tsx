b7c64f544cd2b252645ce06d9f4804df92ed78ad
Marcelo R. Pierre <marcelo.r.pierre@gmail.com>
2021-12-18 21:45:51 +0000

    :construction: Created first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HelloWorld></HelloWorld>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
